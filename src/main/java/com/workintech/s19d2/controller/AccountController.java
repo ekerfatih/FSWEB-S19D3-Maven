22422736ce822df42a714e191e7ed8244225b8fe
Fatih Eker <fatiheker97@gmail.com>
2025-08-13 23:23:03 +0300

    resolved

---
+++ src/main/java/com/workintech/s19d2/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.workintech.s19d2.controller;
+
+import com.workintech.s19d2.entity.Account;
+import com.workintech.s19d2.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/account")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @GetMapping
+    public List<Account> findAll() {
+        return accountService.findAll();
+    }
+
+    @PostMapping
+    public Account save(@RequestBody Account account) {
+        return accountService.save(account);
+    }
+
+
+}
